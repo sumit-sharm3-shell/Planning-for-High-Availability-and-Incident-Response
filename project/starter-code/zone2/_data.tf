efc574ff36363ac6beeffd5df8688e3f022ea16c
sumit-sharm3-shell <86064824+sumit-sharm3-shell@users.noreply.github.com>
2023-10-28 18:21:08 +0530

    Update _data.tf

--- project/starter-code/zone2/_data.tf
+++ project/starter-code/zone2/_data.tf
@@ -19,7 +19,7 @@ data "aws_caller_identity" "current" {}
 data "terraform_remote_state" "vpc" {
   backend = "s3" 
   config = {
-     bucket = "udacity-tf-marta"
+     bucket = "udacity-tf-sharma"
      key    = "terraform/terraform.tfstate"
      region = "us-east-2"
    }
