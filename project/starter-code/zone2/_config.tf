db227815d3c48427a9cb50b3ec9e26c63e80f7f1
sumit-sharm3-shell <86064824+sumit-sharm3-shell@users.noreply.github.com>
2023-10-28 18:07:04 +0530

    Update _config.tf

--- project/starter-code/zone2/_config.tf
+++ project/starter-code/zone2/_config.tf
@@ -1,6 +1,6 @@
 terraform {
    backend "s3" {
-     bucket = "udacity-tf-travis-west"
+     bucket = "udacity-tf-sumit-west"
      key    = "terraform/terraform.tfstate"
      region = "us-west-1"
    }
