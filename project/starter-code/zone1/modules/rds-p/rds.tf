5ffb92a35d37667e868d01bbdb4e00a6a6bd89a0
sumit-sharm3-shell <86064824+sumit-sharm3-shell@users.noreply.github.com>
2023-10-30 12:43:15 +0530

    Update rds.tf

--- project/starter-code/zone1/modules/rds-p/rds.tf
+++ project/starter-code/zone1/modules/rds-p/rds.tf
@@ -61,6 +61,7 @@ output "db_instance_arn" {
 
 resource "aws_rds_cluster_instance" "udacity_instance" {
   count                = 2
+    engine               = "mysql"
   identifier           = "udacity-db-instance-${count.index}"
   cluster_identifier   = aws_rds_cluster.udacity_cluster.id
   instance_class       = "db.t2.small"
